d4019f5ef1229589d12101f401e707de98de26c4
Konstructa <elenprojects7@gmail.com>
2022-02-14 20:31:53 -0300

    generate resouces

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerController } from './customer/customer.controller';
+import { ProductModule } from './product/product.module';
+import { CustomerModule } from './customer/customer.module';
+import { OrderController } from './order/order.controller';
+import { OrderService } from './order/order.service';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductModule, CustomerModule, OrderModule],
+  controllers: [AppController, CustomerController, OrderController],
+  providers: [AppService, OrderService],
 })
 export class AppModule {}
