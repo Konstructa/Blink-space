bd2bd8ef4c380e4629c39feb070737eb17c920dd
Konstructa <elenprojects7@gmail.com>
2022-02-15 00:28:28 -0300

    adicionando modules, controllers e services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CustomerController } from './customer/customer.controller';
 import { ProductModule } from './product/product.module';
 import { CustomerModule } from './customer/customer.module';
 import { OrderController } from './order/order.controller';
 import { OrderService } from './order/order.service';
 import { OrderModule } from './order/order.module';
+import { CustomerService } from './customer/customer.service';
+import { customerProviders } from './customer/entities/customer.providers';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductModule, CustomerModule, OrderModule],
-  controllers: [AppController, CustomerController, OrderController],
-  providers: [AppService, OrderService],
+  imports: [DatabaseModule, ProductModule, CustomerModule, OrderModule],
+  controllers: [CustomerController, OrderController],
+  providers: [...customerProviders, CustomerService, OrderService],
 })
 export class AppModule {}
