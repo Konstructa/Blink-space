9ade074af0611a0bcad0a8e5d82518256a90d56b
Konstructa <elenprojects7@gmail.com>
2022-02-15 00:29:49 -0300

    criando entidade

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,19 @@
-export class Customer {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+}
