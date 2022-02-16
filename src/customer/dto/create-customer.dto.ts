456d10355a365da365450f0091bc5722406b2e5b
Konstructa <elenprojects7@gmail.com>
2022-02-15 22:09:03 -0300

    adicionando class validator

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,6 +1,20 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail({ message: 'Insira um email válido' })
   email: string;
 }
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,15 @@
+import { IsString, IsNotEmpty, IsInt } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString({ message: 'O nome precisa ser uma string' })
+  @IsNotEmpty()
   name: string;
-  quantity: number;
-  value: number;
+
+  @IsString({ message: 'O nome precisa ser uma string' })
+  @IsNotEmpty()
   description: string;
+
+  @IsInt({ message: 'O valor é dado em numeros inteiros!' })
+  @IsNotEmpty()
+  quantity: number;
 }
