4d65463c2c57043d9702a1aa42c3188d2fd24eee
Konstructa <elenprojects7@gmail.com>
2022-02-15 22:07:27 -0300

    adicionando modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,19 @@ import { OrderModule } from './order/order.module';
 import { CustomerService } from './customer/customer.service';
 import { customerProviders } from './customer/entities/customer.providers';
 import { DatabaseModule } from './database/database.module';
+import { productProviders } from './product/entities/product.providers';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
 
 @Module({
   imports: [DatabaseModule, ProductModule, CustomerModule, OrderModule],
-  controllers: [CustomerController, OrderController],
-  providers: [...customerProviders, CustomerService, OrderService],
+  controllers: [CustomerController, OrderController, ProductController],
+  providers: [
+    ...customerProviders,
+    ...productProviders,
+    CustomerService,
+    OrderService,
+    ProductService,
+  ],
 })
 export class AppModule {}
