3d9e53fff91d68b93c23cb1f8fd9b46e2f7db285
Konstructa <elenprojects7@gmail.com>
2022-02-16 01:45:26 -0300

    adicionando costumer e product provider

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { customerProviders } from 'src/customer/entities/customer.providers';
+import { DatabaseModule } from 'src/database/database.module';
+import { productProviders } from 'src/product/entities/product.providers';
+import { orderProviders } from './entities/order.providers';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [
+    ...orderProviders,
+    ...productProviders,
+    ...customerProviders,
+    OrderService,
+  ],
 })
 export class OrderModule {}
