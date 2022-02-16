a3350bb9f0bfb3975b0d16fc487f15414f8b5c45
Konstructa <elenprojects7@gmail.com>
2022-02-16 01:45:37 -0300

    criado order controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.orderService.create(createOrderDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.orderService.remove(+id);
+  }
+
+  @Get('/customerByOrderID/:id')
+  getCustomer(@Param('id') id: string) {
+    return this.orderService.getCustomer(+id);
+  }
+
+  @Get('/productDetailsByOrderID/:id')
+  getProduc(@Param('id') id: string) {
+    return this.orderService.getProduct(+id);
+  }
+}
