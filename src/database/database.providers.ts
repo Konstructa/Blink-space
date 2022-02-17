72a26cd9a3ceb137949357a7bced73ac86c625f8
Konstructa <elenprojects7@gmail.com>
2022-02-17 13:32:51 -0300

    edit

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -10,10 +10,10 @@ export const databaseProviders = [
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: '@aKFfBUvHw4C',
+        password: '',
         database: 'blink_space',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + 'migrations/*{.ts,.js}'],
+        migrations: [__dirname + './migrations/*{.ts,.js}'],
         synchronize: true,
         migrationsRun: true,
         cli: {
